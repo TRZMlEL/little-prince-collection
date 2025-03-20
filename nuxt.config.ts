6020fcc8535105e4ea3dd8a6713159c06e44c1a0
TRZMlEL <mateusztrzmiel2104@gmail.com>
2025-03-20 20:58:50 +0100

    ⭐ Favicon supprt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,16 @@ import glsl from "vite-plugin-glsl";
 export default defineNuxtConfig({
   app: {
     baseURL: '/little-prince-collection/', // Zmień na nazwę repozytorium
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/icon0.svg' },
+        { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/icon1.png' },
+        { rel: 'icon', type: 'image/png', sizes: '512x512', href: '/icon2.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-icon.png' },
+        { rel: 'manifest', href: '/site.webmanifest' }
+      ],
+    },
   },
   routeRules: {
     '/**': { prerender: true } // Generowanie statycznych plików
@@ -19,11 +29,6 @@ export default defineNuxtConfig({
       glsl(),
     ],
   },
-  modules: [
-    'nuxt-rfg-icon',
-    '@nuxtjs/manifest',
-  ],
-
   // nitro: {
   //   prerender: {
   //     crawlLinks: false, // Wyłącza automatyczne znajdowanie stron do prerenderowania
