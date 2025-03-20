d50b2fb1b789fe2a4fad7427691c65120a482627
TRZMlEL <mateusztrzmiel2104@gmail.com>
2025-03-20 21:04:10 +0100

    Remove unused 512x512 favicon icon from nuxt.config.ts and delete icon2.png

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,6 @@ export default defineNuxtConfig({
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
         { rel: 'icon', type: 'image/svg+xml', href: '/icon0.svg' },
         { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/icon1.png' },
-        { rel: 'icon', type: 'image/png', sizes: '512x512', href: '/icon2.png' },
         { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-icon.png' },
         { rel: 'manifest', href: '/site.webmanifest' }
       ],
